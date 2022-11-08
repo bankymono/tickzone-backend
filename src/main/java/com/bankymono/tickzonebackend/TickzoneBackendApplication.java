811848c9f673bf72cb565a071406409100df2e5d
Ayodeji Bankole <Ayodeji.Bankole@Interswitchng.com>
2022-11-08 16:31:11 +0100

    initial commit

---
+++ src/main/java/com/bankymono/tickzonebackend/TickzoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bankymono.tickzonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickzoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickzoneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankymono/tickzonebackend/TickzoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankymono.tickzonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickzoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
